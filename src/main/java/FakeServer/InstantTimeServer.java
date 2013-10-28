cc2bc7846b18b67325314d3c425032b11aad7ab6
Breno Carvalho <brenowcarvalho@gmail.com>
2013-10-28 10:02:36 -0500

    Server says hi

---
+++ src/main/java/FakeServer/InstantTimeServer.java
@@ -0,0 +1,78 @@
+package FakeServer;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import model.Task;
+import Client.IServer;
+import Network.*;
+
+public class InstantTimeServer implements IServer{
+
+	private List<Message> in_messages, out_messages;
+	private boolean busy;
+	
+	public InstantTimeServer(){
+		busy = false;
+		in_messages = new LinkedList<Message>();
+		out_messages = new LinkedList<Message>();
+	}
+	
+	public void remove() {
+		out_messages.remove(0);		
+	}
+
+	// TODO change this raw type to the client type, and put randomness
+	public void request(TaskMessage message) {
+		Task task = message.getTask();
+		if(!busy){
+			busy = true;
+			Message out = null;
+			if(in_messages.isEmpty()){
+				try {
+					long begin = System.nanoTime();
+					task.run();
+					//System.out.printf("Elapsed: %d nano sec\n", System.nanoTime()-begin);
+					out = new SuccessMessage<IServer>(this, task);
+				} catch (Exception e) {
+					// TODO create error message
+					out = new FailMessage<IServer>(this, task);
+				}
+				out_messages.add(out);
+			}else{
+				while(!in_messages.isEmpty()){
+					task = ((TaskMessage) in_messages.remove(0)).getTask();
+					try {
+						task.run();
+						out = new SuccessMessage<IServer>(this, task);
+					} catch (Exception e) {
+						out = new FailMessage<IServer>(this, task);
+						continue;
+					}
+					out_messages.add(out);
+				}
+			}
+			busy = false;
+		}else{
+			in_messages.add(message);
+		}
+		
+	}
+
+	public void getStatus() {
+		// TODO implement method to inform if server is online
+		
+	}
+	public boolean isBusy(){
+		return busy;
+	}
+
+	public boolean hasNext() {
+		return !out_messages.isEmpty();
+	}
+
+	public Message next() {
+		return out_messages.remove(0);
+	}
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,54 +1,28 @@
 import model.*;
-import parsing.Parser;
+import Client.*;
+import FakeServer.*;
+import Network.*;
 
 public class Main {
 	public static void main(String args[]) throws Exception{
+		
+		IServer server = new InstantTimeServer();
+		server.request(new TaskMessage<Object>(null, new Task("Hi",1)));
+		if(server.hasNext()){
+			System.out.print(((SuccessMessage) server.next()).getTask().getName());
+		}
+		
+		/*
 		if( args.length < 2){
 			throw new Exception("Bad parameters. I need a filename and a threads number.");
 		}
 		Parser parser = new Parser();
 		parser.parse(args[0]);
 		Graph g = parser.getGraph();
-		/*
-		Graph g = new Graph();
-		Command t = new CreateTaskCommand(g);
-		int wait = 500;
-		t.run("t1 := sleep "+wait);
-		t.run("t2 := sleep "+wait);
-		t.run("t3 := sleep "+wait);
-		t.run("t4 := sleep "+wait);
-		t.run("t5 := sleep "+wait);
-		Command u = new ChainTasksCommand(g);
-		u.run("t1 -> t2 -> t3 -> t5");
-		u.run("t2 -> t4 -> t5");
-		System.out.println(g.toString());
-		
-		/*
-		Node p,q, r = null;
-		
-		p = new Node(new Task("T1", wait), g);
-		for(int i = 2; i < 7; i++){
-			q = new Node(new Task("T"+i, wait), g);
-			if( i == 3){
-				r = q;
-			}
-			q.addParent(p);
-			//System.out.print("["+q.toString()+" " +g.contains(q)+"]\n");
-			p = q;
-		}
-		q = new Node(new Task("T5.5", wait), g);
-		p.addParent(q); // T5.5 must run first than T6
-		q.addParent(r); // T3 must run first than T5.5
-		//JOptionPane.showMessageDialog(null, g.contains(q)+" "+q.getNumParents()+" "+r.getTaskName()+" "+r.getNumChildren());
-		*/
-		//System.out.println(g);
-		
-		
 		Scheduler sc = new Scheduler(Integer.valueOf(args[1]).intValue(), g);
 		sc.execute();
-		//System.out.println("Orphan tasks: "+g.hasOrphan());
 		Thread.sleep(1000);
 		System.out.println(sc);
-		//JOptionPane.showMessageDialog(null, g.contains(q)+" "+q.getNumParents()+" "+r.getTaskName()+" "+r.getNumChildren());
+		*/
 	}
 }
